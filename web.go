a1aa6d358bbf98784f3e44827684b8b9f7bea157
wangzz2019 <jack.wang@datadoghq.com>
2019-09-18 19:46:18 +0900

    add test

--- web.go
+++ web.go
@@ -17,6 +17,7 @@ func main() {
 
 	// Routes
 	e.GET("/", hello)
+	e.GET("/test", test)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
@@ -25,4 +26,7 @@ func main() {
 // Handler
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World! This is jack's first echo web!")
+}
+func test(c echo.Context) error {
+	return c.String(http.StatusOK, "Hi, This is a test page")
 }
